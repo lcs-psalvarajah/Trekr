8a633aa869580117a49e1b93e06fd961f0372ead
jina <psalvarajah@lcs.on.ca>
2022-01-18 09:25:43 -0500

    Initial Commit

---
+++ Shared/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Shared
+//
+//  Created by Salvarajah, Prajina on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
