670ef70d75e000d60fd6283dd9e541813e7301c4
jina <psalvarajah@lcs.on.ca>
2022-01-19 10:01:03 -0500

    Added the tips and map view, then added a tab bar to switch between them and the location list

---
+++ Shared/Views/TipsList.swift
@@ -0,0 +1,20 @@
+//
+//  TipsList.swift
+//  Trekr
+//
+//  Created by Salvarajah, Prajina on 2022-01-19.
+//
+
+import SwiftUI
+
+struct TipsList: View {
+    var body: some View {
+        Text("Hello, TipsList View!")
+    }
+}
+
+struct TipsList_Previews: PreviewProvider {
+    static var previews: some View {
+        TipsList()
+    }
+}
---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,20 @@
+//
+//  WorldMap.swift
+//  Trekr
+//
+//  Created by Salvarajah, Prajina on 2022-01-19.
+//
+
+import SwiftUI
+
+struct WorldMap: View {
+    var body: some View {
+        Text("Hello, World Map view!")
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldMap()
+    }
+}
